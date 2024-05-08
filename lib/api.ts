b2ab9d3447da1ed66f164ff2b7b9ef5fc63af87c
kzares-dev <138066850+kzares-dev@users.noreply.github.com>
2024-05-07 23:02:36 -0400

    dev env setup

---
+++ lib/api.ts
@@ -0,0 +1,10 @@
+//? the api used for this application is the free dictionary api
+const baseUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+export async function getWord(word: string) {
+
+
+
+
+
+}
